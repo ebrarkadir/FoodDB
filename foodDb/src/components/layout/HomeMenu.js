4715f17d2792a76fd8fbeaf640abd632a174d7b0
Oguzhan Yildiz <oguzyildiz.3536@gmail.com>
2024-05-14 19:58:12 +0300

    foto_fix

--- foodDb/src/components/layout/HomeMenu.js
+++ foodDb/src/components/layout/HomeMenu.js
@@ -17,10 +17,22 @@ export default function HomeMenu() {
     <section className="">
       <div className="absolute left-0 right-0 w-full justify-start">
         <div className="absolute left-0 -top-[70px] text-left -z-10">
-          <Image src={'/sallad1.png'} width={109} height={189}  alt={'sallad'} />
+<Image
+  src={'/sallad1.png'}
+  width={200}
+  height={300}
+  alt={'sallad'}
+  style={{ transform: 'rotate(37deg)' }}
+/>
         </div>
         <div className="absolute -top-[100px] right-0 -z-10">
-          <Image src={'/sallad2.png'} width={107} height={195} alt={'sallad'} />
+        <Image
+  src={'/sallad2.png'}
+  width={250}
+  height={350}
+  alt={'sallad'}
+  style={{ transform: 'rotate( - 37deg)' }}
+/>
         </div>
       </div>
       <div className="text-center mb-4">
