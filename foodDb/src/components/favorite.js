b7cc7f187cf882878de49587df68b62c8f6fdeb5
ebrarkadir <cetinebrarkadir@gmail.com>
2024-05-22 20:18:31 +0300

    ebrarkadir

---
+++ foodDb/src/components/favorite.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const favorite = ({ productId }) => {
+    const [isFavorite, setIsFavorite] = useState(() => {
+        const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+        return storedFavorites.includes(productId);
+      });
+    
+      const handleFavorite = () => {
+        const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+        let updatedFavorites;
+    
+        if (isFavorite) {
+          updatedFavorites = storedFavorites.filter(id => id !== productId);
+        } else {
+          updatedFavorites = [...storedFavorites, productId];
+        }
+    
+        localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+        setIsFavorite(!isFavorite);
+      };
+    
+      return (
+        <button onClick={handleFavorite}>
+          {isFavorite ? 'Favorilerden Çıkar' : 'Favorilere Ekle'}
+        </button>
+      );
+}
+
+export default favorite;
