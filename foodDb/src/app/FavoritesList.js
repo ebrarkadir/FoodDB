3488219f54ea5147d21eb7eaeb419487f0c72773
ebrarkadir <cetinebrarkadir@gmail.com>
2024-05-30 22:28:48 +0300

    favorite_fix

--- foodDb/src/app/FavoritesList.js
+++ foodDb/src/app/FavoritesList.js
@@ -1,7 +1,5 @@
-"use client";
 import React, { useEffect, useState } from 'react';
 import SectionHeaders from "@/components/layout/SectionHeaders";
-import MenuItem from "@/components/menu/MenuItem";
 
 const FavoritesList = () => {
   const [favorites, setFavorites] = useState([]);
@@ -11,16 +9,24 @@ const FavoritesList = () => {
     setFavorites(storedFavorites);
   }, []);
 
+  const removeFromFavorites = (itemName) => {
+    const updatedFavorites = favorites.filter(name => name !== itemName);
+    setFavorites(updatedFavorites);
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+  };
+
   if (favorites.length === 0) {
     return <p>Henüz favorilere eklenmiş bir ürün yok.</p>;
   }
 
   return (
     <section className="my-16">
-
       <div className="grid sm:grid-cols-3 gap-4">
         {favorites.map((name) => (
-          <MenuItem key={name} name={name} /> // MenuItem bileşeninin uygun propsları olduğundan emin olun
+          <div key={name} className="border p-4">
+            <p>{name}</p>
+            <button onClick={() => removeFromFavorites(name)}>Favorilerden Kaldır</button>
+          </div>
         ))}
       </div>
     </section>
